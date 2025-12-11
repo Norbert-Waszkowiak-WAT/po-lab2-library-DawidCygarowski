6a74c0410f895e8e0e57b4fe63e03726b87d1088
DawidCygarowski <dcygarowski@gmail.com>
2025-12-11 18:43:37 +0000

    Zadanie lab2

--- book.cpp
+++ book.cpp
@@ -7,15 +7,13 @@ class Book{
     string title;
     string author;
     string isbn;
-
     public:
     Book(string title, string author, string isbn):
-    title(title),author(author),isbn(isbn){}
-    ~Book(){cout<<"Destroying book: " << title <<endl;}
+    title(title), author(author), isbn(isbn){}
+    ~Book(){cout << "Destroying book: " << title << endl;}
     string getTitle(){return title;}
     string getAuthor(){return author;};
     string getIsbn(){return isbn;}
-
-
+ 
 };
 #endif
